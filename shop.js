1bf39ccde5307698ca16db6bf9d3bcda46260a6c
zack! <zaczac021021@gmail.com>
2022-07-24 11:39:46 -0700

    creat mini shop themplete

---
+++ shop.js
@@ -0,0 +1,9 @@
+var i=document.getElementById("i");
+var form=document.getElementById("form");
+i.onclick=function(){
+alert("hellow");
+BarProp=false;
+}
+const myModal = new bootstrap.Modal('#myModal', {
+    keyboard: false
+  })
