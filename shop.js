2ff4b224cdcc46cb47f8fd82bc0da4041438d68d
zack! <zaczac021021@gmail.com>
2022-07-24 15:49:31 -0700

    change banner

--- shop.js
+++ shop.js
@@ -1,7 +1,6 @@
 var i=document.getElementById("i");
 var form=document.getElementById("form");
 i.onclick=function(){
-alert("hellow");
 BarProp=false;
 }
 const myModal = new bootstrap.Modal('#myModal', {
